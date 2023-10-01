6b18a660895b990fa7eed28edf2edfacb47d696f
Роман Семашкевич <semaroman@mail.ru>
2023-10-01 20:49:44 +0300

    Commit message

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,25 @@
 package ru.netology;
 
-public class Main {
+import java.util.function.*;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Calculator calc = Calculator.instance.get();
+            int a = calc.plus.apply(1, 2);
+            int b = calc.minus.apply(1, 1);
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль невозможно");
+        }
     }
+}
+
+class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    Consumer<Integer> println = System.out::println;
 }
