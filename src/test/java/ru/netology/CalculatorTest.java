497bdb7732049022922e2e461846a7e769ef6608
Роман Семашкевич <semaroman@mail.ru>
2023-10-03 10:43:22 +0300

    Доработка

--- src/test/java/ru/netology/CalculatorTest.java
+++ src/test/java/ru/netology/CalculatorTest.java
@@ -4,26 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-    int x = 1;
-    int y = 2;
+    int x = 6;
+    int y = 3;
 
     @Test
     void plusTest() {
-        int expected = x + y;
+        int expected = 9;
         int actual = Calculator.plus.apply(x, y);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void minusTest() {
-        int expected = x - y;
+        int expected = 3;
         int actual = Calculator.minus.apply(x, y);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void devideTest() {
-        int expected = x / y;
+        int expected = 2;
         int actual = Calculator.devide.apply(x, y);
         Assertions.assertEquals(expected, actual);
     }
